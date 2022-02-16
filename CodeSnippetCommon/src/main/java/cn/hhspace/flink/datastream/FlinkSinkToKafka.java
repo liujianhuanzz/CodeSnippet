349a819cd11024cffa7cb9851034db445cd149a1
liujianhuan <liujianhuan@qianxin.com>
2022-02-16 18:17:47 +0800

    add flink datastream sink mysql

--- CodeSnippetCommon/src/main/java/cn/hhspace/flink/datastream/FlinkSinkToKafka.java
+++ CodeSnippetCommon/src/main/java/cn/hhspace/flink/datastream/FlinkSinkToKafka.java
@@ -4,6 +4,10 @@ import cn.hhspace.flink.datastream.sink.FlinkEsOutputProcessFunction;
 import cn.hhspace.flink.datastream.sink.FlinkKafkaProducerFatory;
 import cn.hhspace.flink.datastream.source.SensorReading;
 import cn.hhspace.flink.datastream.source.SensorSource;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -39,6 +43,33 @@ public class FlinkSinkToKafka {
         //写入kafka
         readings.addSink(FlinkKafkaProducerFatory.getFlinkKafkaProducer());
         //readings.print();
+
+        DataStream<SensorReading> filter = readings.filter(new FilterFunction<SensorReading>() {
+            @Override
+            public boolean filter(SensorReading sensorReading) throws Exception {
+                return sensorReading.temperature > 100;
+            }
+        });
+
+        filter.addSink(JdbcSink.sink(
+                "insert into flink_test_mysql (id, timestamp, temperature) values (?, ? ,?)",
+                (stmt, sr) -> {
+                    stmt.setString(1, sr.id);
+                    stmt.setLong(2, sr.timestamp);
+                    stmt.setDouble(3, sr.temperature);
+                },
+                JdbcExecutionOptions.builder()
+                        .withBatchSize(1000)
+                        .withBatchIntervalMs(200)
+                        .withMaxRetries(5)
+                        .build(),
+                new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                        .withUrl("jdbc:mysql://hostname:3306/nice")
+                        .withDriverName("com.mysql.jdbc.Driver")
+                        .withUsername("root")
+                        .withPassword("x")
+                        .build()
+        ));
 
         env.execute("Learning Flink Sink To Kafka");
     }
